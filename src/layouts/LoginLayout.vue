cfdde81d7969cc4467992d2b872d50bfeaa7a769
Tottibero <tottiesp@gmail.com>
2025-01-02 16:24:21 +0100

    change layouts

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex min-h-screen bg-gray-100">
+      <main class="m-auto p-6 bg-white shadow-md rounded">
+        <slot />
+      </main>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    name: 'LoginLayout',
+  };
+  </script>
+  
