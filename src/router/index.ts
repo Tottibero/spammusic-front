2f6f2d131a2a07a27854d3e2067605ca62894c98
Tottibero <tottiesp@gmail.com>
2025-01-02 10:50:22 +0100

    cambios

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import type { RouteRecordRaw } from 'vue-router';
-import { useAuthStore } from '../stores/auth';
+import { createRouter, createWebHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
+import { useAuthStore } from "../stores/auth";
 
-import Login from '../views/Login.vue';
-import Home from '../views/Home.vue';
+import Login from "../views/Login.vue";
+import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
     meta: { requiresAuth: true }, // Indica que esta ruta requiere autenticación
   },
   {
-    path: '/page1',
-    name: 'Page1',
-    component: () => import('../views/Page1.vue'),
+    path: "/page1",
+    name: "Page1",
+    component: () => import("../views/Page1.vue"),
     meta: { requiresAuth: true }, // Indica que esta ruta requiere autenticación
   },
   {
-    path: '/page2',
-    name: 'Page2',
-    component: () => import('../views/Page2.vue'),
+    path: "/page2",
+    name: "Page2",
+    component: () => import("../views/Page2.vue"),
     meta: { requiresAuth: true }, // Indica que esta ruta requiere autenticación
   },
   {
-    path: '/login',
-    name: 'Login',
+    path: "/login",
+    name: "Login",
     component: Login,
   },
 ];
@@ -37,15 +37,16 @@ const router = createRouter({
 });
 
 // Global before guard
-router.beforeEach((to, from, next) => {
+router.beforeEach((to) => {
   const authStore = useAuthStore();
 
   // Verifica si la ruta requiere autenticación
   if (to.meta.requiresAuth && !authStore.isAuthenticated) {
-    next({ name: 'Login' }); // Redirige al login si no está autenticado
-  } else {
-    next(); // Permite el acceso
+    return { name: "Login" }; // Redirige al login si no está autenticado
   }
+
+  // Permite el acceso
+  return true;
 });
 
 export default router;
