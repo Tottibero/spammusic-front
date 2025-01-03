b5cfaf8da3f9477bfa7b5978009d1120ace13e74
Tottibero <tottiesp@gmail.com>
2025-01-03 19:58:23 +0100

    arreglados los estilos

--- src/services/rates.ts
+++ src/services/rates.ts
@@ -9,3 +9,9 @@ export interface RatePayload {
 export async function postRate(payload: RatePayload): Promise<void> {
   await api.post('/rates', payload);
 }
+
+export async function updateRate(rateId: string, updateRateDto: any) {
+  // PATCH /rates/:id
+  console.log("update rate", updateRateDto)
+  return api.patch(`/rates/${rateId}`, updateRateDto);
+}
