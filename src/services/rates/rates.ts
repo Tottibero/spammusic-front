f9a09f6549b737c61ed483225b7e4bb0844fe9b8
Tottibero <tottiesp@gmail.com>
2025-01-24 18:07:51 +0100

    mis votos

--- src/services/rates/rates.ts
+++ src/services/rates/rates.ts
@@ -1,4 +1,5 @@
 import api from "@services/api/api.ts";
+import type { Disc } from "@services/discs/discs";
 
 export interface RatePayload {
   discId: string;
@@ -20,18 +21,37 @@ export interface DiscRate {
   rate: string;
   cover: string;
   user: User;
+  disc?: Disc;
 }
 
-
 export async function postRateService(payload: any): Promise<void> {
-  const response = await api.post('/rates', payload);
+  const response = await api.post("/rates", payload);
   return response.data;
 }
 
-export async function updateRateService( ratingId: string, updateRateDto: any) {
+export async function updateRateService(ratingId: string, updateRateDto: any) {
   // PATCH /rates/:id
-  console.log("update rate", updateRateDto)
+  console.log("update rate", updateRateDto);
   return api.patch(`/rates/${ratingId}`, updateRateDto);
+}
+
+export async function getRatesByUser(
+  limit: number,
+  offset: number,
+  query?: string,
+  dateRange?: any,
+  genre?: string,
+): Promise<DiscRate[]> {
+  const response = await api.get<DiscRate[]>("/rates", {
+    params: {
+      limit,
+      offset,
+      query, // Incluye el query en los parámetros si está definido
+      dateRange,
+      genre,
+    },
+  });
+  return response.data;
 }
 
 export async function getDiscRates(discId: string): Promise<DiscRate[]> {
