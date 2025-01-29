0d13137d01a527b898d948158f4aa513fc5f701b
Tottibero <tottiesp@gmail.com>
2025-01-29 09:52:56 +0100

    reuniones

--- src/stores/auth/auth.ts
+++ src/stores/auth/auth.ts
@@ -6,6 +6,7 @@ export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: localStorage.getItem('token') as string | null,
     username: localStorage.getItem('username') as string | null,
+    userId: localStorage.getItem('userId') as string | null, // Almacenar el ID del usuario
   }),
   actions: {
     async login(payload: LoginPayload) {
@@ -13,8 +14,11 @@ export const useAuthStore = defineStore('auth', {
         const response = await login(payload);
         this.token = response.token;
         this.username = response.username;
+        this.userId = response.id; // Guardar el ID del usuario en el estado
+        console.log("store userId: " + this.userId);
         localStorage.setItem('token', response.token);
         localStorage.setItem('username', response.username);
+        localStorage.setItem('userId', response.id); // Guardar el ID en localStorage
 
         // Configura el token en Axios para futuras solicitudes
         api.defaults.headers.common['Authorization'] = `Bearer ${response.token}`;
@@ -26,8 +30,10 @@ export const useAuthStore = defineStore('auth', {
     logout() {
       this.token = null;
       this.username = null;
+      this.userId = null; // Limpiar el ID del estado
       localStorage.removeItem('token');
       localStorage.removeItem('username');
+      localStorage.removeItem('userId'); // Remover el ID de localStorage
 
       // Elimina el token de las cabeceras globales
       delete api.defaults.headers.common['Authorization'];
@@ -41,5 +47,9 @@ export const useAuthStore = defineStore('auth', {
   },
   getters: {
     isAuthenticated: (state): boolean => !!state.token,
+    loggedUser: (state) => ({ // Nuevo getter para obtener el usuario logueado
+      id: state.userId,
+      username: state.username,
+    }),
   },
 });
