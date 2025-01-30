b29e04388623f136599c7b5a93ccce65c11f5129
Tottibero <tottiesp@gmail.com>
2025-01-30 19:49:32 +0100

    botton spotify

--- src/components/SpotifyArtistButton.vue
+++ src/components/SpotifyArtistButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     @click="openArtistLink"
-    class="bg-green-500 text-white px-4 py-2 rounded ml-2"
+    class="bg-green-500 text-white px-2 py-1 rounded ml-2"
   >
     <i class="fa-brands fa-spotify"></i>
   </button>
