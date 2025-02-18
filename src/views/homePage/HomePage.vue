b0995a4dde583055ba324325c2273285551be396
Tottibero <tottiesp@gmail.com>
2025-02-18 21:51:08 +0100

    mes

--- src/views/homePage/HomePage.vue
+++ src/views/homePage/HomePage.vue
@@ -1,17 +1,28 @@
 <template>
-  <div :class="{ 'menu-open': menuVisible }" class="max-w-[100rem] mx-auto mt-10 px-4">
+  <div
+    :class="{ 'menu-open': menuVisible }"
+    class="max-w-[100rem] mx-auto mt-10 px-4"
+  >
     <div class="bg-white shadow-lg rounded-lg p-6 mb-10">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
         <!-- Estadísticas de la App -->
-        <div class="text-center border-b md:border-b-0 md:border-r border-gray-200 pb-4 md:pb-0 md:pr-4">
-          <h3 class="text-3xl font-bold mb-4 text-gray-900">Estadísticas de discos</h3>
+        <div
+          class="text-center border-b md:border-b-0 md:border-r border-gray-200 pb-4 md:pb-0 md:pr-4"
+        >
+          <h3 class="text-3xl font-bold mb-4 text-gray-900">
+            Estadísticas de discos
+          </h3>
           <p class="text-lg text-gray-900">
             Total de discos:
-            <span class="font-semibold text-gray-900">{{ stats.totalDiscs }}</span>
+            <span class="font-semibold text-gray-900">{{
+              stats.totalDiscs
+            }}</span>
           </p>
           <p class="text-lg mt-2">
             Total de votos:
-            <span class="font-semibold text-gray-900">{{ stats.totalVotes }}</span>
+            <span class="font-semibold text-gray-900">{{
+              stats.totalVotes
+            }}</span>
           </p>
           <!-- Aquí insertamos el gráfico de barras -->
           <div class="mt-6">
@@ -22,7 +33,9 @@
         <div class="text-center">
           <h3 class="text-3xl font-bold mb-4 text-gray-900">Top usuarios</h3>
           <div class="mb-4">
-            <h4 class="text-2xl font-semibold mb-2 text-gray-900">Más discos votados</h4>
+            <h4 class="text-2xl font-semibold mb-2 text-gray-900">
+              Más discos votados
+            </h4>
             <ul class="list-none">
               <li
                 v-for="(user, index) in topUsersByRates"
@@ -31,12 +44,16 @@
               >
                 <span class="mr-2" v-html="getTrophyIcon(index)"></span>
                 <span class="font-medium">{{ user.user.username }}</span>
-                <span class="ml-2 text-gray-600">- {{ user.rateCount }} votos</span>
+                <span class="ml-2 text-gray-600"
+                  >- {{ user.rateCount }} votos</span
+                >
               </li>
             </ul>
           </div>
           <div>
-            <h4 class="text-2xl font-semibold mb-2 text-gray-900">Más portadas votadas</h4>
+            <h4 class="text-2xl font-semibold mb-2 text-gray-900">
+              Más portadas votadas
+            </h4>
             <ul class="list-none">
               <li
                 v-for="(user, index) in topUsersByCover"
@@ -45,7 +62,9 @@
               >
                 <span class="mr-2" v-html="getTrophyIcon(index)"></span>
                 <span class="font-medium">{{ user.user.username }}</span>
-                <span class="ml-2 text-gray-600">- {{ user.totalCover }} votos</span>
+                <span class="ml-2 text-gray-600"
+                  >- {{ user.totalCover }} votos</span
+                >
               </li>
             </ul>
           </div>
@@ -60,29 +79,57 @@
     <!-- Botones de filtro por fecha -->
     <div class="flex justify-center space-x-4 mb-6">
       <button
-        @click="selectedPeriod = 'week'; fetchDiscs()"
-        :class="selectedPeriod === 'week' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'"
+        @click="
+          selectedPeriod = 'week';
+          fetchDiscs();
+        "
+        :class="
+          selectedPeriod === 'week'
+            ? 'bg-blue-500 text-white'
+            : 'bg-gray-200 text-gray-700'
+        "
         class="px-4 py-2 rounded"
       >
         Semana
       </button>
       <button
-        @click="selectedPeriod = 'month'; fetchDiscs()"
-        :class="selectedPeriod === 'month' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'"
+        @click="
+          selectedPeriod = 'month';
+          fetchDiscs();
+        "
+        :class="
+          selectedPeriod === 'month'
+            ? 'bg-blue-500 text-white'
+            : 'bg-gray-200 text-gray-700'
+        "
         class="px-4 py-2 rounded"
       >
         Mes
       </button>
       <button
-        @click="selectedPeriod = 'year'; fetchDiscs()"
-        :class="selectedPeriod === 'year' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'"
+        @click="
+          selectedPeriod = 'year';
+          fetchDiscs();
+        "
+        :class="
+          selectedPeriod === 'year'
+            ? 'bg-blue-500 text-white'
+            : 'bg-gray-200 text-gray-700'
+        "
         class="px-4 py-2 rounded"
       >
         Año
       </button>
       <button
-        @click="selectedPeriod = 'all'; fetchDiscs()"
-        :class="selectedPeriod === 'all' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'"
+        @click="
+          selectedPeriod = 'all';
+          fetchDiscs();
+        "
+        :class="
+          selectedPeriod === 'all'
+            ? 'bg-blue-500 text-white'
+            : 'bg-gray-200 text-gray-700'
+        "
         class="px-4 py-2 rounded"
       >
         Todos
@@ -152,7 +199,12 @@ export default defineComponent({
       if (period === "week") {
         startDate.setDate(endDate.getDate() - 7);
       } else if (period === "month") {
-        startDate.setMonth(endDate.getMonth() - 1);
+        const startDate = new Date(today.getFullYear(), today.getMonth(), 1);
+        const endDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+        return [
+          startDate.toISOString().split("T")[0],
+          endDate.toISOString().split("T")[0],
+        ];
       } else if (period === "year") {
         startDate.setFullYear(endDate.getFullYear() - 1);
       }
@@ -167,7 +219,8 @@ export default defineComponent({
     const fetchDiscs = async () => {
       try {
         const dateRange = getDateRange(selectedPeriod.value);
-        const response: DiscsStatsResponse = await getTopRatedOrFeaturedAndStats(dateRange);
+        const response: DiscsStatsResponse =
+          await getTopRatedOrFeaturedAndStats(dateRange);
         discs.value = response.discs;
         stats.value.totalDiscs = response.totalDiscs;
         stats.value.totalVotes = response.totalVotes;
