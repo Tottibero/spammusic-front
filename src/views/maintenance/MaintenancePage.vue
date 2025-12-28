75d9eb078f467e8b13f4b982aaebf29ca3f6c98e
tottibero <tottiesp@gmail.com>
2025-12-28 14:07:25 +0100

    cambios layout

--- src/views/maintenance/MaintenancePage.vue
+++ src/views/maintenance/MaintenancePage.vue
@@ -22,7 +22,8 @@
                 Estamos realizando mejoras en <span class="font-bold text-primary">Riff Valley</span> para brindarte una
                 mejor experiencia.
                 <br>
-                Por favor, vuelve a intentarlo en unos minutos.
+                Se informará por el canal <a href="https://t.me/spammusicapp" target="_blank"
+                    class="text-blue-500 hover:underline">Telegram</a>.
             </p>
 
             <!-- Botón Recargar -->
@@ -40,7 +41,7 @@
             <div class="mt-8 pt-6 border-t border-gray-100">
                 <p class="text-sm text-gray-400">
                     ¿Necesitas ayuda urgente? Contáctanos en
-                    <a href="https://t.me/+82736120501" target="_blank"
+                    <a href="https://t.me/+zVadIcsfRKw2ZmQ0" target="_blank"
                         class="text-blue-500 hover:underline">Telegram</a>
                 </p>
             </div>
